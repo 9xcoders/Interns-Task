36ca6ce4411d17c57b3e9c3f7e8f8f0c6d0cf37d
Kasun Sameera <49017278+klksl@users.noreply.github.com>
2021-01-21 13:37:39 +0530

    Add files via upload
    onedu.org website

---
+++ index.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OnEdu Org</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link  rel="stylesheet" href="css/menu.css" type="text/css" />
+
+	<link rel="stylesheet" href="https://anijs.github.io/lib/anicollection/anicollection.css">
+
+    </head>
+</head>
+<body class="headline">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+
+            <ul id="nav">
+				<li><a href="index.php"><img src="img/logo.webp" alt="logo" style="height:35px;"></a></li>
+                <li><a class="hsubs" href="#section1">Home</a></li> 
+                <li><a class="hsubs" href="#blog-area">News</a></li>
+                <li><a class="hsubs" href="#testimornial-area">Courses</a></li>
+				<li><a class="hsubs" href="courseapply.php">Apply Courses</a></li>
+				<li><a class="hsubs" href="#aboutus">About Us</a></li>
+                <li><a class="hsubs" href="#section5">Contact Us</a></li></li>
+				<div id="lavalamp"></div>
+				
+            </ul>
+
+        </div>
+	</nav>    
+
+	<div id="section1">
+		<div class="container">
+			<h2>Xx Educational Organization</h2>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			  </ol>
+		  
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner">
+		  
+				<div class="item active">
+				  <img src="img/bg/bg1.jpg" alt="Los Angeles" style="width:100%; ">
+				  <div class="carousel-caption">
+					<h3>Build Your Future</h3>
+					<p>------------------</p>
+				  </div>
+				</div>
+		  
+				<div class="item">
+				  <img src="img/bg/bg2.jpg" alt="Chicago" style="width:100%; ">
+				  <div class="carousel-caption">
+					<h3>Success is not final, failure is not fatal</h3>
+					<p>------------------</p>
+				  </div>
+				</div>
+			  
+				<div class="item">
+				  <img src="img/bg/bg3.jpg" alt="New York" style="width:100%; ">
+				  <div class="carousel-caption">
+					<h3>The goal is to die with memories not dreams</h3>
+					<p>------------------</p>
+				  </div>
+				</div>
+			
+			  </div>
+		  
+			  <!-- Left and right controls -->
+			  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			  </a>
+			</div>
+		  </div>
+
+	</div>
+	<div id="section2">
+		<!-- Start Feature Area -->
+		<section id="feature-area" class="about-section">
+			<div class="container">
+				<div class="row text-center inner">
+					<div class="col-sm-4">
+						<div class="feature-content">
+							<img src="img/abt_us.jpg" alt="Image">
+							<h2 class="feature-content-title green-text">About onEdu.org</h2>
+							<p class="feature-content-description">onEdu.org is an international higher education institution. Our mission is to provide transformative educational opportunities that combine academic excellence, professional skills and leadership development.
+							</p>
+							<!-- <a href="#" class="feature-content-link green-btn">Read More</a> -->
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="feature-content">
+							<img src="img/stdymeth.jpg" alt="Image">
+							<h2 class="feature-content-title blue-text">Study Methods</h2>
+							<p class="feature-content-description">onedu.org is provide online video lessons and also online lecture sessions via our own platform. All the registered students can learn and complete your courses through those study methods.</p>
+								<!-- <a href="#" class="feature-content-link blue-btn">See Courses</a> -->
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="feature-content">
+							<img src="img/resrs.jpg" alt="Image">
+							<h2 class="feature-content-title red-text">Learning Resources</h2>
+							<p class="feature-content-description">A key learning resource for everyone studying at Inntech College is the College's virtual learning environment (VLE).If you are a current student and need help to access the VLE please email our support team at info@oneduorg.com.</p>
+							<!-- <a href="#" class="feature-content-link red-btn">Button Red</a> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Feature Area -->
+
+		<!-- Start Blog Area -->
+		<section id="blog-area">
+			<div class="container">
+				<div align="center">
+					<h3 class="myh3">*All Of Our Courses Are*</h3>					
+				</div>
+				<div class="row text-center inner">
+					<div class="col-sm-6">
+						<div class="blog-content">
+							<img src="img/2-1.jpg" alt="Image">
+							<h2 class="ex2">Online & Flexible - So you can do them anytime </h2>
+							<p>Our courses are all online and self-paced, meaning you can take them wherever and whenever it’s convenient for you. So whether you’re in your breakroom at work during lunch or it’s 9PM and you’ve just put your kids to bed, these courses were designed to seamlessly fit into your life.</p><br>
+
+							</div>
+						</div>
+						
+						<div class="col-sm-6">
+							<div class="blog-content">
+								<img src="img/2-2.jpg" alt="Image">
+								<h2 class="ex2">Mentor-Led-So you'll always have someone to turn on</h2>
+								<p>All of our courses are led by an instructor. With weekly office hours, the opportunity to receive feedback on completed work, you’ll always have the support you need, when you need it.<br><br>
+								</p>
+									<!-- <span><a href="#section1">Home</a></span><br> -->
+									<p id="blofr"></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- End Blog Area -->
+			</div>
+			<div id="section3">
+				<!-- Start Services Area -->
+				<section id="services-area" class="services-section">
+					<div class="container">
+							<div class="row mb-5">
+								<div class="col-md-6 col-lg-3">
+								  <div class="ftco-footer-widget mb-5">
+									
+									<h2 class="h2my">Vision</h2>
+									<div class="block-21 mb-4 d-flex">
+										<p class="pmy">Setting the global standard in online and digital learning.</p>
+									</div>
+								  </div>
+								</div>
+								<div class="col-md-6 col-lg-3">
+								  <div class="ftco-footer-widget mb-5">
+									<h2 class="h2my">Mission</h2>
+									<p class="pmy">Creating community and knowledge around quality online, blended, and digital learning while driving innovation.</p>
+								  
+								  </div>
+								</div>
+								<div class="col-md-6 col-lg-3">
+								  <div class="ftco-footer-widget mb-5 ml-md-4">
+									<img src="img/inforeq.jpg" alt="online" >
+
+								  </div>
+								</div>
+								<div class="col-md-6 col-lg-3">
+								  <div class="ftco-footer-widget mb-5">
+			
+										<h2 class="h2my">Request Information</h2>
+										<p class="pmy">Thank you for your interest about us.
+										<br>Do you want more details?? Click below button for request more information of onEdu.org.</p>
+				
+										<br>
+										<a href="requestinfo.php" class="feature-content-link green-btn">Request info</a>
+								  </div>
+								
+								</div>
+							  </div>
+						</div>
+					</section>
+					<!-- End Services Area -->
+
+					<!-- Start Testimornial Area -->
+					
+					<section id="testimornial-area">
+						<div class="container">
+							<div class="row text-center">
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img class="course-img" src="img/crses/web.png" alt="Image" >
+										<h2 class="ex2">Web Development</h2>
+										
+										<p>In this course you will be professionally instructed on how to create a professionally optimized website in terms of design, SEO and security</p>
+										<div id="main" data-anijs="if: mouseover, do: swing animated"> 
+										<a href="course1.html" class="content-link">read more</a>
+										</div>
+										<br>
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img class="course-img" src="img/crses/android.png" alt="Image">
+										<h2 class="ex2">Android Development</h2>
+										<p>Get a firm grip on Android Programming.Learn to Find solutions to any problem related to Android Programming.
+											Appreciate the power of Open Source !</p>
+											<div id="main" data-anijs="if: mouseover, do: swing animated"> 
+										<a href="course2.html" class="content-link">read more</a>
+										</div>
+										<br>
+										<p id="redd"></p>
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img class="course-img" src="img/crses/test.jpg" alt="Image">
+										<h2 class="ex2">Selenium Test Automation</h2>
+										<p>Selenium is one of the best alternatives for those who want to automate functional testing of web projects.</p>
+										<div id="main" data-anijs="if: mouseover, do: swing animated"> 
+										<a href="course3.html" class="content-link">read more</a>
+										</div>
+										<br>
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img class="course-img" src="img/crses/ios.jpeg" alt="Image">
+										<h2 class="ex2">iOS App Deveopment</h2>
+										<p>In this course you will learn through several projects to develop apps for iPhone and iPad, from scratch.</p>
+										<div id="main" data-anijs="if: mouseover, do: swing animated"> 
+										<a href="course4.html" class="content-link">read more</a>
+										</div>
+										<br>
+										<p id="dets"></p>
+									</div>
+								</div>
+							</div>
+							<div class="row" id="aboutus">
+								<div class="col-lg-12">
+									<div class="tm-box">
+										<img src="img/Untitled-1.jpg" alt="Image" class="img-responsive">
+										<div class="tm-box-description">
+											<h2>About Us</h2>
+											<p class="tm-box-p">Starting out as Sloan Consortium (Sloan-C) in 1999, we have been the leading professional online learning society devoted to advancing quality e-Education into the mainstream of education through its community. In 2014, as a self-sustaining worldwide organization, we rebranded to the Online Learning Consortium (OLC) to better align with our mission: Creating community and knowledge around quality online, blended, and digital learning while driving innovation.</p>
+											<p class="tm-box-p">Twenty Years (1992-2012) of the Alfred P. Sloan Foundation’s Anytime, Anyplace Learning Program
+
+												This website is the result of a research and evaluation project documenting the contributions of the Alfred P. Sloan Foundation’s Anytime, Anyplace Learning Program.</p>
+											<!-- <a href="#" class="content-link">Read More</a>     -->
+										</div>                        
+									</div>                    
+								</div>
+							</div>
+						</div>
+					</section>
+					<!-- End Testimornial Area -->
+				</div>
+
+					<!-- Start Footer Area -->
+					<div id="section5">
+					<footer id="footer-area">
+							<div class="container">
+								<div class="row mb-5">
+								  <div class="col-md-6 col-lg-3">
+									<div class="ftco-footer-widget mb-5">
+										<h2 class="h2my">Have Questions?</h2>
+										<div class="block-23 mb-3">
+										  <ul>
+											<li><span class="icon icon-map-marker"></span><span class="text">Glenn House, Houston Park, Montford Street, Manchester, M50 2RP, United Kingdom</span></li>
+											<li><span class="icon icon-phone"></span><span class="text">+44 (0) 711834260</span></li>
+											<li><span class="icon icon-envelope"></span><span class="text">info@oneduorg.com</span></li>
+										  </ul>
+										</div>
+									</div>
+								  </div>
+
+								  <div class="col-md-6 col-lg-3">
+									<div class="ftco-footer-widget mb-5 ml-md-4">
+									  <h2 class="h2my">Links</h2>
+									  <ul class="list-unstyled">
+										<li><a href=""><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+										<li><a href=""><span class="ion-ios-arrow-round-forward mr-2"></span>News</a></li>
+										<li><a href=""><span class="ion-ios-arrow-round-forward mr-2"></span>Courses</a></li>
+										<li><a href=""><span class="ion-ios-arrow-round-forward mr-2"></span>About Us</a></li>
+										
+									  </ul>
+									</div>
+								  </div>
+								  <div class="col-md-6 col-lg-3">
+									<div class="ftco-footer-widget mb-5">
+										<h2 class="h2my">Subscribe Us!</h2>
+									  <form action="#" class="subscribe-form">
+										<div class="form-group">
+										  <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
+										  <input type="submit" value="Subscribe" class="form-control submit px-3">
+										</div>
+									  </form>
+									</div>
+								  
+								  </div>
+								</div>
+								<div class="row">
+								  <div class="col-md-12 text-center">
+						
+									<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved onEdu.org | Registered in the UK as company</p>
+						  <p>number 11778318</p>
+								  </div>
+								</div>
+							  </div>
+						</footer>
+					</div>
+						<!-- End Footer Area -->
+
+						<script src="js/jquery-1.11.2.min.js"></script>
+						<script src="js/jquery.scrollUp.min.js"></script> <!-- https://github.com/markgoodyear/scrollup -->
+						<script src="js/jquery.singlePageNav.min.js"></script> <!-- https://github.com/ChrisWojcik/single-page-nav -->
+						<script src="js/parallax.js-1.3.1/parallax.js"></script> <!-- http://pixelcog.github.io/parallax.js/ -->
+
+						<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+  						<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+						<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+						<!-- AniJS core library -->
+  <script src="https://anijs.github.io/lib/anijs/anijs-min.js"></script> 
+  
+  <!-- Include to use $addClass, $toggleClass or $removeClass -->
+  <script src="https://anijs.github.io/lib/anijs/helpers/dom/anijs-helper-dom-min.js"></script>
+    <!-- Include scrollReveal -->
+	<script src="https://unpkg.com/scrollreveal@4"></script>
+	<script>
+	  ScrollReveal().reveal('.headline');
+  </script>
+
+						  
+						<script>
+
+    // HTML document is loaded. DOM is ready.
+    $(function() {  
+
+    // Parallax
+        // $('.intro-section').parallax({
+        // 	imageSrc: 'img/bg-1.jpg',
+        // 	speed: 0.2
+        // });
+
+        $('.services-section').parallax({
+        	imageSrc: 'img/bg-2.jpg',
+        	speed: 0.2
+    	});
+        $('.contact-section').parallax({
+        	imageSrc: 'img/bg-3.jpg',
+        	speed: 0.2
+        });    
+
+        // jQuery Scroll Up / Back To Top Image
+        $.scrollUp({
+                scrollName: 'scrollUp',      // Element ID
+		        scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+		        scrollFrom: 'top',           // 'top' or 'bottom'
+		        scrollSpeed: 1000,            // Speed back to top (ms)
+		        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+		        animation: 'fade',           // Fade, slide, none
+		        animationSpeed: 300,         // Animation speed (ms)		        
+		        scrollText: '', // Text for element, can contain HTML		        
+		        scrollImg: true            // Set true to use image		        
+            });
+
+        // ScrollUp Placement
+        $( window ).on( 'scroll', function() {
+
+            // If the height of the document less the height of the document is the same as the
+            // distance the window has scrolled from the top...
+            if ( $( document ).height() - $( window ).height() === $( window ).scrollTop() ) {
+
+                // Adjust the scrollUp image so that it's a few pixels above the footer
+                $('#scrollUp').css( 'bottom', '80px' );
+
+            } else {      
+                // Otherwise, leave set it to its default value.
+                $('#scrollUp').css( 'bottom', '30px' );        
+            }
+        });
+
+        $('.single-page-nav').singlePageNav({
+        	offset: $('.single-page-nav').outerHeight(),
+        	speed: 1500,
+        	filter: ':not(.external)',
+        	updateHash: true
+        });
+
+        $('.navbar-toggle').click(function(){
+        	$('.single-page-nav').toggleClass('show');
+        });
+
+        $('.single-page-nav a').click(function(){
+        	$('.single-page-nav').removeClass('show');
+        });
+        
+    });
+</script>
+</body>
+</html>
